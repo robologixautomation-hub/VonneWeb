d3ecd92549b536b987417ca8ae29fe21f50474cf
aaronibarra <aibarra.cepeda@gmail.com>
2026-09-17 00:19:35 -0600

    Compilacion de Tailwind CSS a styles.prod.css y precarga de imagen LCP para rendimiento movil optimo

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+﻿module.exports = {
+  content: ["./index.html", "./app.js"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#faf7f2',
+          100: '#f4efe8',
+          200: '#ebdcd0',
+          300: '#dfc299',
+          400: '#c5a880',
+          500: '#b59265',
+          600: '#8c7355',
+          700: '#68543d',
+          800: '#38342f',
+          900: '#1e1b18',
+        }
+      }
+    }
+  }
+}
